08ef0eda2834ea2e459b47e3e924badc5f756b13
Akash-m-SE <akashm9923@gmail.com>
2024-05-21 10:59:00 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,81 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "my-ec2-instance" {
+  ami                                  = var.ami
+  associate_public_ip_address          = true
+  availability_zone                    = "eu-central-1a"
+  disable_api_stop                     = false
+  disable_api_termination              = false
+  ebs_optimized                        = false
+  get_password_data                    = false
+  hibernation                          = false
+  host_id                              = null
+  host_resource_group_arn              = null
+  iam_instance_profile                 = null
+  instance_initiated_shutdown_behavior = "stop"
+  instance_type                        = var.instance_type
+  ipv6_address_count                   = 1
+  key_name                             = "vaultkey"
+  monitoring                           = false
+  placement_group                      = null
+  placement_partition_number           = 0
+  private_ip                           = "172.31.24.91"
+  secondary_private_ips                = []
+  security_groups                      = ["launch-wizard-3"]
+  source_dest_check                    = true
+  subnet_id                            = var.subnet_id
+  tags = {
+    Name = "Existing Ec2 Instance to Terraform"
+  }
+  tags_all = {
+    Name = "Existing Ec2 Instance to Terraform"
+  }
+  tenancy                     = "default"
+  user_data                   = null
+  user_data_base64            = null
+  user_data_replace_on_change = null
+  volume_tags                 = null
+  vpc_security_group_ids      = ["sg-03b70b382c89f5278"]
+  capacity_reservation_specification {
+    capacity_reservation_preference = "open"
+  }
+  cpu_options {
+    amd_sev_snp      = null
+    core_count       = 1
+    threads_per_core = 1
+  }
+  credit_specification {
+    cpu_credits = "standard"
+  }
+  enclave_options {
+    enabled = false
+  }
+  maintenance_options {
+    auto_recovery = "default"
+  }
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_protocol_ipv6          = "disabled"
+    http_put_response_hop_limit = 2
+    http_tokens                 = "required"
+    instance_metadata_tags      = "disabled"
+  }
+  private_dns_name_options {
+    enable_resource_name_dns_a_record    = true
+    enable_resource_name_dns_aaaa_record = false
+    hostname_type                        = "ip-name"
+  }
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = false
+    iops                  = 3000
+    kms_key_id            = null
+    tags                  = {}
+    tags_all              = {}
+    throughput            = 125
+    volume_size           = 8
+    volume_type           = "gp3"
+  }
+}
